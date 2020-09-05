26c6e5e9badf365d5dbee90442e1905f2dd4ec4c
Yongguang Zhu <Yongguang.Zhu@microsoft.com>
2020-09-05 11:01:50 +0800

    use C#8 syntax

--- Downloader.cs
+++ Downloader.cs
@@ -32,37 +32,33 @@ namespace Zyg.PowerToys
             var fileName = req.Query["name"];
             if (string.IsNullOrEmpty(fileName)) fileName = Path.GetFileName(fileUrl);
 
-            using (var client = new WebClient())
+            using var client = new WebClient();
+            var sw = Stopwatch.StartNew();
+            await using var stream = await client.OpenReadTaskAsync(fileUrl);
+            var blob = fileContainer.GetBlockBlobReference(fileName);
+            var length = 0;
+            await using (var blobStream = await blob.OpenWriteAsync())
             {
-                var sw = Stopwatch.StartNew();
-                using (var stream = await client.OpenReadTaskAsync(fileUrl))
+                const int BUFFER_SIZE = 128 * 1024;
+                var buffer = new byte[BUFFER_SIZE];
+                int bytesRead;
+
+                do
                 {
-                    var blob = fileContainer.GetBlockBlobReference(fileName);
-                    var length = 0;
-                    using (var blobStream = await blob.OpenWriteAsync())
-                    {
-                        const int BUFFER_SIZE = 128 * 1024;
-                        var buffer = new byte[BUFFER_SIZE];
-                        int bytesRead;
-
-                        do
-                        {
-                            bytesRead = await stream.ReadAsync(buffer, 0, BUFFER_SIZE);
-                            length += bytesRead;
-                            await blobStream.WriteAsync(buffer, 0, bytesRead);
-                        } while (bytesRead > 0);
-                    }
-                    stream.Close();
-
-                    sw.Stop();
-                    return new OkObjectResult(new
-                    {
-                        url = blob.Uri.AbsoluteUri,
-                        size = length,
-                        time = sw.ElapsedMilliseconds / 1000
-                    });
-                }
+                    bytesRead = await stream.ReadAsync(buffer, 0, BUFFER_SIZE);
+                    length += bytesRead;
+                    await blobStream.WriteAsync(buffer, 0, bytesRead);
+                } while (bytesRead > 0);
             }
+            stream.Close();
+
+            sw.Stop();
+            return new OkObjectResult(new
+            {
+                url = blob.Uri.AbsoluteUri,
+                size = length,
+                time = sw.ElapsedMilliseconds / 1000
+            });
         }
     }
 }
